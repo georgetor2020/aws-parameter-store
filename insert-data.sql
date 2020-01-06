53a1aaef103e1c6a4195cb1332d73086344c7362
Dave <cdd@gmail.com>
2020-01-06 09:00:35 -0600

    correct INSERT syntax error

--- insert-data.sql
+++ insert-data.sql
@@ -1,4 +1,3 @@
 use DevDB;
 
-INSERT INTO "customer" ("customer_id","first_name","last_name","street_address","city","state","zip") 
-  VALUES ("1","Jane","Smith","1 South Main","Springfield","OH","43215");  
+INSERT INTO customer (customer_id,first_name,last_name,street_address,city,state,zip)   VALUES ("1","Jane","Smith","1 South Main","Springfield","OH","43215");  
